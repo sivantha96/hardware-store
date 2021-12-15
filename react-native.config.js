ae699920df25f75f6016b28d3c7f2d0fe4c5d858
Sivantha <Sivantha.paranavithana@gmail.com>
2021-12-16 00:12:00 +0530

    Add fonts

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+/**
+ * configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+    project: {
+        ios: {},
+        android: {},
+    },
+    assets: ['./app/assets/fonts'],
+};
