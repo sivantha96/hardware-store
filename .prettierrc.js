aa4baabc86ef183431ba467595bbf01643374458
Sivantha <Sivantha.paranavithana@gmail.com>
2021-12-16 00:10:53 +0530

    Add config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    tabWidth: 4,
+    printWidth: 100,
+    endOfLine: 'auto',
 };
